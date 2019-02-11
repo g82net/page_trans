6cfcad117f46498a4390896b763efed2fbc309ca
g82net <47095143+g82net@users.noreply.github.com>
2019-02-11 18:23:57 +0900

    data.dart > add to 5pages and Comments

--- lib/data.dart
+++ lib/data.dart
@@ -10,8 +10,15 @@ class IntroItem {
   final String imageUrl;
 }
 
+//geolocation info
+//getWeather info
+//get data for dust info
+
 final sampleItems = <IntroItem>[
   new IntroItem(title: 'Fine Dust', category: 'AIR POLLUTION', imageUrl: 'assets/1.png',),
   new IntroItem(title: 'Occasionally wearing pants is a good idea.', category: 'WEATHER', imageUrl: 'assets/2.png',),
   new IntroItem(title: 'We might have the best team spirit ever.', category: 'SPIRIT', imageUrl: 'assets/3.png',),
+  new IntroItem(title: 'We might have the best team spirit ever.', category: 'CAMERA', imageUrl: 'assets/Back2.png',),
+  new IntroItem(title: 'We might have the best team spirit ever.', category: 'GALLERY', imageUrl: 'assets/_back1.png',),
 ];
+
